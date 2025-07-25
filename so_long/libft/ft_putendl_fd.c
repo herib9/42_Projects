24a48d7b7ba4a7bb24c1c0a7b5ec155206ccc65a
herib9 <157405740+herib9@users.noreply.github.com>
2025-07-25 23:50:02 +0200

    Add files via upload

---
+++ so_long/libft/ft_putendl_fd.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmolina <hmolina@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 19:27:19 by hmolina           #+#    #+#             */
+/*   Updated: 2025/07/15 22:00:35 by hmolina          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*escribe una cadena de caracteres seguida de un salto de línea (\n) en un
+file descriptor específico. Es útil para escribir líneas completas en archivos o
+flujos de salida, agregando automáticamente el salto de línea al final*/
+
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	write(fd, s, ft_strlen(s));
+	write(fd, "\n", 1);
+}
+
+/*int	main(void)
+{
+	int	fd;
+
+	ft_putendl_fd("Hello World", 1);
+	fd = open ("out_endl.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd != -1)
+	{
+		ft_putendl_fd("Hello World", fd);
+		close(fd);
+	}
+	return (0);
+}*/
