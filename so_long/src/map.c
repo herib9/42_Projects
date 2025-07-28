821ca219423ff25b3c5e931647fc5f9d9a1acc69
herib9 <157405740+herib9@users.noreply.github.com>
2025-07-28 02:28:39 +0200

    Add files via upload

--- so_long/src/map.c
+++ so_long/src/map.c
@@ -1,23 +1,44 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   map.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hmolina <<hmolina@student.42.fr>>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/23 19:41:19 by hmolina           #+#    #+#             */
-/*   Updated: 2025/07/25 00:24:17 by hmolina          ###   ########.fr       */
+/*   Created: 2024/07/23 19:41:19 by hmolina           #+#    #+#             */
+/*   Updated: 2025/07/28 02:19:19 by hmolina          ###   ########lyon.fr   */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "so_long.h"
+
+static char	**asignement_n_read_lines(int fd)
+{
+	char	**map;
+	char	*line;
+	int		i;
+
+	map = malloc(sizeof(char *) * 100);
+	if (!map)
+		return (NULL);
+	i = 0;
+	while ((line == get_next_line(fd)) != NULL)
+	{
+		if (line && line[ft_strlen(line) - 1] == '\n')
+			line[ft_strlen(line) - 1] = '\0';
+		map[i] = line;
+		i++;
+		if (i >= 99)
+			break ;
+	}
+	map[i] = NULL;
+	return (map);
+}
 
 char	**read_map(char *filename)
 {
 	char	**map;
-	char	*line;
-	int	fd;
-	int	i;
+	int		fd;
 
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
@@ -25,42 +46,30 @@ char	**read_map(char *filename)
 		perror("Error, read failure");
 		return (NULL);
 	}
-	map = malloc(sizeof(char*) * 100);
+	map = asignement_n_read_lines(fd);
+	close(fd);
 	if (!map)
 	{
-		close(fd);
+		perror("Error allocating memory");
 		return (NULL);
 	}
-	i = 0;
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		if (line && line[ft_strlen(line) - 1] == '\n')
-			line[ft_strlen(line) - 1] = '\0';
-		map[i] = line;
-		i++;
-		if (i >= 99)
-			break;
-	}
-	map[i] = NULL;
-	close(fd);
 	return (map);
 }
 
-void	error(char **map)
+int	map_loading_error(char **map, char *filename)
 {
-	int	y;
-	int	x;
-	
-	y = 0;
-	while(map[y] != 0)
+	if (!map)
 	{
-		x = 0;
+		ft_printf("Error\nFailed to load map: %s\n", filename);
+		return (0);
 	}
+	return (1);
 }
 
 void	free_map(char **map)
 {
 	int	i;
+
 	if (!map)
 		return ;
 	i = 0;
@@ -74,5 +83,14 @@ void	free_map(char **map)
 
 void	calculate_map_size(t_game *game)
 {
+	int	height;
 
+	height = 0;
+	while (game->map[height])
+		height++;
+	game->map_height = height;
+	if (height > 0)
+		game->map_width = ft_strelen(game->map[0]);
+	else
+		game->map_width = 0;
 }
--- so_long/src/player.c
+++ so_long/src/player.c
@@ -1,4 +1,4 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   player.c                                           :+:      :+:    :+:   */
@@ -6,29 +6,88 @@
 /*   By: hmolina <<hmolina@student.42.fr>>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 00:57:00 by hmolina           #+#    #+#             */
-/*   Updated: 2025/07/24 05:33:59 by hmolina          ###   ########.fr       */
+/*   Updated: 2025/07/28 02:17:08 by hmolina          ###   ########lyon.fr   */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "so_long.h"
 
 void	keymap(int keycode, t_game *game)
 {
-	t_game	*game;
+	if (keycode == KEY_ESCAPE)
+		close_handler(game);
+	else if (keycode == KEY_S || keycode == KEY_DOWN)
+		move_down(game, 0, -1);
+	else if (keycode == KEY_D || keycode == KEY_RIGHT)
+		move_right(game, 1, 0);
+	else if (keycode == KEY_A || keycode == KEY_LEFT)
+		move_left(game, -1, 0);
+	else if (keycode == KEY_W || keycode == KEY_UP)
+		move_up(game, 0, -1);
+}
 
-	game = (t_game *)param;
-	if (keydata.action == MLX_PRESS)
+void	fing_player_position(t_game *game)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < game->map_height)
 	{
-		if (keydata.key == MLX_KEY_W || keydata.key == MLX_KEY_UP)
-			move_up(game);
-		else if (keydata.key == MLX_KEY_S || keydata.key == MLX_KEY_DOWN)
-			move_down(game);
-		else if (keydata.key == MLX_KEY_D || keydata.key == MLX_KEY_RIGHT)
-			move_right(game);
-		else if (keydata.key == MLX_KEY_A || keydata.key == MLX_KEY_LEFT)
-			move_left(game);
-		else if (keydata.key == MLX_KEY_ESCAPE)
-			mlx_close_window(game->mlx)
+		x = 0;
+		while (x < game->map_width)
+		{
+			if (game->map[y][x] == PLAYER)
+			{
+				game->player_x = x;
+				game->player_y = y;
+				return ;
+			}
+			x++;
+		}
+		y++;
 	}
 }
 
+void	find_exit_position(t_game *game)
+{
+	int	x;
+	int	y;
+	
+	y = 0;
+	while (y < game->map_height)
+	{
+		x = 0;
+		while (x < game->map_width)
+		{
+			if (game->map[y][x] == EXIT)
+			{
+				game->exit_x = x;
+				game->exit_y = y;
+				return ;
+			}
+			x++;
+		}
+		y++;
+	}
+}
+
+void	count_collectibles(t_game *game)
+{
+	int	x;
+	int	y;
+
+	game->total_totallectibles = 0;
+	y = 0;
+	while (y < game->map_height)
+	{
+		x = 0;
+		while (x < game->map_width)
+		{
+			if (game->map[y][x] == COLLECTIBLE)
+				game->total_collectibles++;
+			x++;
+		}
+		y++;
+	}
+}
