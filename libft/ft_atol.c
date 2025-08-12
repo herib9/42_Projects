8184ee2f0f71e1b3603e623c2d4e0b4732a7e7ee
herib9 <157405740+herib9@users.noreply.github.com>
2025-08-12 23:57:10 +0200

    Add files via upload

---
+++ libft/ft_atol.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmolina <<hmolina@student.42.fr>>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/12 21:42:54 by hmolina           #+#    #+#             */
+/*   Updated: 2025/08/12 23:52:18 by hmolina          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+//es igual que atoi, pero contempla numeros mas pequeños y mas grandes
+
+#include "libft.h"
+
+long	ft_atol(char *s)
+{
+	long	i;
+	long	sig;
+	long	num;
+	
+	i = 0;
+	sig = 1;
+	num = 0;
+	while (s[i] == '\t' || s[i] == '\n' || s[i] == '\r' || s[i] == '\f' ||
+		s[i] == '\v' || s[i] == ' ')
+		i++;
+	if (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sig = -1;
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		num = num * 10 + (s[i] - '0');
+		i++;
+	}
+	return (sig * num);
+}
