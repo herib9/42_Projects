4a7820e119542ff0b2463a95283be1d8925189ed
herib9 <157405740+herib9@users.noreply.github.com>
2026-05-18 17:25:13 +0200

    Add files via upload

---
+++ CPP/CPP02/ex00/Fixed.cpp
@@ -0,0 +1,30 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _fPvalue(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other) {
+	std::cout << "Copy constructor called" << std::endl;
+	this->_fPvalue = other.getRawBits();
+}
+
+Fixed &Fixed::operator=(const Fixed &other) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if(this != &other)
+		this->_fPvalue = other.getRawBits();
+	return *this;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_fPvalue;
+}
+
+void	Fixed::setRawBits(int const raw) {
+	this->_fPvalue = raw;
+}
---
+++ CPP/CPP02/ex00/Fixed.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class	Fixed
+{
+	private:
+		int					_fPvalue;
+		static const int	_bits = 8;
+
+	public:
+		Fixed();
+		Fixed(const Fixed &other);
+		Fixed &operator=(const Fixed &other);
+		~Fixed();
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+};
