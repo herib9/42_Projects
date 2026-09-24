9291fb77f8a5717dbb5daf6841c915573e834ffe
herib9 <157405740+herib9@users.noreply.github.com>
2026-09-24 21:22:16 +0200

    Add files via upload

--- CPP/CPP05/ex00/Bureaucrat.cpp
+++ CPP/CPP05/ex00/Bureaucrat.cpp
@@ -1,2 +1,23 @@
 #include "Bureaucrat.hpp"
 
+Bureaucrat::Bureaucrat(): name(""){
+	this->grade = 150;
+}
+
+Bureaucrat::Bureaucrat(std::string const name, int grade): name(name){
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	else
+		this->grade = grade;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat& original): name (original.name){
+	if (this != &original)
+		*this = original;
+}
+
+Bureaucrat::Bureaucrat& operator (const Bureaucrat &other){
+	
+}
--- CPP/CPP05/ex00/Bureaucrat.hpp
+++ CPP/CPP05/ex00/Bureaucrat.hpp
@@ -1,12 +1,33 @@
 #pragma once
+#include <exception>
 #include <iostream>
 
 class	Bureaucrat
 {
 	private:
-
+		std::string const name;
+		int grade;
 
 	public:
+		Bureaucrat();
+		Bureaucrat(std::string const name, int grade);
+		Bureaucrat(Bureaucrat& original);
+		~Bureaucrat();
+		Bureaucrat& operator = (const Bureaucrat &other);
 
-	
+		std::string getName() const;
+		int		getGrade() const;
+		void	increment();
+		void	decrement();
+		class	GradeTooHighException: public std::exception{
+			public:
+				const char *what() const throw();
+		};
+		class	GradeTooLowException: public std::exception{
+			public:
+				const char *what() const throw();
+		};
 };
+
+std::ostream& operator << (std::ostream& out, const Bureaucrat& b);
+
